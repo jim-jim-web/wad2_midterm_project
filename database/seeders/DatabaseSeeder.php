3dcb9cdc263886f539a0cda688c5125b22f4eb0d
Jehu Casimiro <j.casimiro@qstrike.com>
2026-03-03 09:36:52 +0800

    added student CRUD

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Student;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,6 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+        Student::factory(200)->create();
 
         User::factory()->create([
             'name' => 'Test User',
